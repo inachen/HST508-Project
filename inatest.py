a55f4b307f778fbe4a32a0f24159839665184c5e
ichen <weilinchen@college.harvard.edu>
2013-12-05 14:23:28 -0500

    sync

--- inatest.py
+++ inatest.py
@@ -1,3 +1,5 @@
+# inatest.py
+# -------------------------------------------
 # testing random stuff
 
 import sys
@@ -6,8 +8,6 @@ import numpy as np
 import collections
 import copy
 from scipy import integrate
-import constants as c
-from utils import *
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
