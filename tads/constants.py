67dde5049a183b765c0430ca96826c512c1baa26
ichen <weilinchen@college.harvard.edu>
2013-12-11 14:33:46 -0500

    shifted files around

---
+++ tads/constants.py
@@ -0,0 +1,28 @@
+# constants.py
+# -------------------------------------------
+# constants for Hi-C project
+
+# Hi-C data folder
+DATAPATH = "/Users/inachen/Dropbox/binned_heatmaps/"
+
+# folder to write output
+OUTPATH = "output/"
+
+# number of chromosomes in human cells
+NUM_CHRMS = 23
+
+# default window size
+WIN_SIZE = 50
+
+# default threshold for read quality
+QUAL_THRESH = 0.75
+
+# default threshold for boundary shift 
+SHIFT_THRESH = 550
+
+# default distance a max peak has to be above previous min
+DEL_MIN = 1000
+
+# minimum distance between the min and the max
+MIN_DIST = 50
+
